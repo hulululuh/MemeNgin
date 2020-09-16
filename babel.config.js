1cc88b3b30bc3666cef2c446b994401b35a5a743
Jaemoon Choi <hulululuh@gmail.com>
2020-09-16 14:10:33 +0900

    debug on VSCode(Main & Renderer Process), need to check why typescript are not bound.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  presets: ["@vue/app"]
+  presets: ["@vue/app"],
+  env: {
+    development: {
+      sourceMaps: true,
+      retainLines: true,
+    },
+  },
 };
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 // vue.config.js
 
 module.exports = {
-	pluginOptions: {
-		electronBuilder: {
-			chainWebpackMainProcess: config => {
-				config.plugins.delete("uglify");
-			},
-			chainWebpackRendererProcess: config => {
-				config.plugins.delete("uglify");
-			}
-		}
-	}
+  configureWebpack: {
+    devtool: "source-map",
+  },
+  pluginOptions: {
+    electronBuilder: {
+      chainWebpackMainProcess: (config) => {
+        config.plugins.delete("uglify");
+      },
+      chainWebpackRendererProcess: (config) => {
+        config.plugins.delete("uglify");
+      },
+    },
+  },
 };
