7b8d70d0c83e8d51305efaf75707ed4081027b2a
hulululuh <hulululuh@gmail.com>
2021-07-02 15:39:41 +0900

    handling <a href> click to open external browser.

--- src/App.vue
+++ src/App.vue
@@ -263,6 +263,13 @@
     }
 
     created() {
+      const shell = require("electron").shell;
+      //open links externally by default
+      $(document).on("click", 'a[href^="http"]', function(event) {
+        event.preventDefault();
+        shell.openExternal(this.href);
+      });
+
       UserData.serialize();
 
       electron.ipcRenderer.on(MenuCommands.FileNew, (evt, arg) => {
