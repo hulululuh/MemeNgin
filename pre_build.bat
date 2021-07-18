f7ba699c047f807019ac7fb5dd7f93dd0f1c2d20
hulululuh <hulululuh@gmail.com>
2021-07-18 14:56:40 +0900

    Update pre_build.bat

--- pre_build.bat
+++ pre_build.bat
@@ -1,5 +1,5 @@
 :: 1. copy steamworks_sdk folder into greenworks/deps
-xcopy .\steamworks_sdk .\node_modules\greenworks\deps\steamworks_sdk\ /E/H/Y
+xcopy .\external_deps\steamworks_sdk\ .\node_modules\greenworks\deps\steamworks_sdk\ /E/H/Y
 
 :: 2. build
 ::./node_modules/.bin/electron-rebuild.cmd
